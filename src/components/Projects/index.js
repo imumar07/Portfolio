8edcbdfbd8e99f7a5dcf83687aa92f36c5f38ae9
SYED UMAR <umar630934@gmail.com>
2024-07-02 12:47:52 +0530

    fix: Added hyperlink to diabetes project

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -103,6 +103,7 @@ const projectsList = [
       category: 'AI',
       imageURL: 'https://res.cloudinary.com/dqe4ld4cx/image/upload/v1715449444/Untitled_design_uktqkb.png',
       title: 'Prediction Of Diabetes',
+      projectLink:"https://github.com/imumar07/diabetes_prediction.git",
       description: 'AI based diabetes prediction tool aids early detection.',
     },
   ];
